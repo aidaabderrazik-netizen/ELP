33360c0dff3fe7ce06a7a352f6de2b836d002626
ruth-mlmb <mboumbaemmanuella@gmail.com>
2026-01-08 12:21:23 +0100

    Add files via upload
    Nouveau main

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var graph = map[int][]int{
+	0: {1, 3},
+	1: {0, 2, 4},
+	2: {1, 5},
+	3: {0, 4, 6},
+	4: {1, 3, 5},
+	5: {2, 4, 8},
+	6: {3, 7},
+	7: {6, 8},
+	8: {5, 7, 9},
+	9: {8},
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	//paramètre du test
+	numWalks := 10000
+	duree := 2 * time.Second // 2 minutes
+	startNode := 0
+
+	fmt.Printf("=== Demo : 1 goroutine qui effectue la marche aléatoire pendant %d secondes ===\n", duree/1000000000)
+	//Création du canal pour les results
+	results1 := make(chan []int, numWalks)
+
+	//Création du canal pour savoir quand le process est fini
+	done1 := make(chan bool)
+
+	//Compteur global
+	nodeCounts1 := make(map[int]int)
+
+	var wg1 sync.WaitGroup
+
+	go resultsPro(results1, nodeCounts1, done1)
+	wg1.Add(1)
+	go walkWorker(startNode, duree, results1, &wg1)
+
+	//fermeture du canal
+	go func() {
+		wg1.Wait()
+		close(results1)
+	}()
+	start := time.Now()
+	//Attente du traitement du processeur
+	<-done1 //en gros attente de la lecture du channel
+	fmt.Println("Temps écoulé :", time.Since(start))
+
+	//Affichage des resultats
+	fmt.Println("Nombre de visites par noeud :")
+	total1 := 0
+	for node1, count1 := range nodeCounts1 {
+		fmt.Printf("Noeud %d -> %d visites\n", node1, count1)
+		total1 += count1
+	}
+	fmt.Println("Total visits:", total1)
+
+	//Calculer et afficher les probabilités
+
+	probs1 := ComputeProbabilities(nodeCounts1)
+
+	fmt.Println("\nProbabilités estimées avec 1 goroutine :")
+	for node, p := range probs1 {
+		fmt.Printf("Noeud %d -> %.4f\n", node, p)
+	}
+
+	fmt.Printf("\n=== Demo : %d goroutines qui effectue la marche aléatoire pendant %d secondes ===\n", numWalks, duree/1000000000)
+	//Création du canal pour les results
+	results := make(chan []int, numWalks)
+
+	//Création du canal pour savoir quand le process est fini
+	done := make(chan bool)
+
+	//Compteur global
+	nodeCounts := make(map[int]int)
+
+	var wg sync.WaitGroup
+
+	go resultsPro(results, nodeCounts, done)
+	for i := 0; i < numWalks; i++ {
+		wg.Add(1)
+		go walkWorker(startNode, duree, results, &wg)
+
+	}
+	//fermeture du canal
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+	staart := time.Now()
+	//Attente du traitement du processeur
+	<-done //en gros attente de la lecture du channel
+	fmt.Println("Temps écoulé :", time.Since(staart))
+
+	//Affichage des resultats
+	fmt.Println("Nombre de visites par noeud :")
+	total := 0
+	for node, count := range nodeCounts {
+		fmt.Printf("Noeud %d -> %d visites\n", node, count)
+		total += count
+	}
+	fmt.Println("Total des visites:", total)
+
+	//Calculer et afficher les probabilités
+
+	probs := ComputeProbabilities(nodeCounts)
+
+	fmt.Printf("\nProbabilités estimées avec %d goroutines: \n", numWalks)
+	for node, p := range probs {
+		fmt.Printf("Noeud %d -> %.4f\n", node, p)
+	}
+
+}
