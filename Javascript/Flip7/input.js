11c1efd26d9e4e70f385202ee12b7140cca1e62e
rmalambamb <ruth.malamba-mboumba@insa-lyon.fr>
2026-01-28 10:38:56 +0100

    Code qui demande

--- Javascript/Flip7/input.js
+++ Javascript/Flip7/input.js
@@ -1,6 +1,6 @@
-const readline = require("readline")
+import { createInterface } from "readline";
 
-const rl =readline.createInterface({input: process.stdin, output :process.stdout}) //permet de recuperer la demande de l'utilisateur
+const rl =createInterface({input: process.stdin, output :process.stdout}) //permet de recuperer la demande de l'utilisateur
 
 
 // fonction qui pinermet d'afficher une question
@@ -14,9 +14,21 @@ function askQuestion(question) {
 
 
 
+// Savoir les intentions d'un joueur 
+async function askPlayerChoice(playerName){
+    let choice ;
+    do {
+        choice = await askQuestion(`${playerName}, tirer (d) ou s'arrêter (s) ?`);
+    } while (choice !== "d" && choice !=="s")
+    return choice ;
+    }
+
+
+
+
 function closeInput() {
   rl.close();
 }
 
-module.exports = { askQuestion, closeInput };
+export default { askQuestion, closeInput };
 //retourne "draw" ou "stop"
