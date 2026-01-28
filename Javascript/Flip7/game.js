84a7375665f636f531a4aaf479d5008ee9ef7252
ruth-mlmb <mboumbaemmanuella@gmail.com>
2026-01-28 11:55:01 +0100

    Update game.js

--- Javascript/Flip7/game.js
+++ Javascript/Flip7/game.js
@@ -1,10 +1,29 @@
-startGame()
+// qui joue ? quand 
+const { askQuestion } = require("./input")
+const { constructor } = require("./player")
+
+async function startGame(){
+    let players = [];
+    //combiien de joueurs 
+    console.log("=========Démarrage du jeu Flip7=========")
+    const nb_joueurs= parseInt(await askQuestion("Il y'a combien de joueurs ?"),10);
+    console.log("Nombre de joueurs=", nb_joueurs);
+    for (let id=0 ; id<nb_joueurs; id++) {
+        const player_name = await askQuestion(`Nom du joueur n° ${id+1}:`);
+        players.push(constructor(player_name));
+        
+
+    }
+    console.log("Le jeu commence ")
+}
 
 
-playTurn(player)
+//playTurn(player)
 
 
-endGame()
+//endGame()
 
 
-isGameOver()
+//isGameOver()
+
+module.exports = { startGame}
