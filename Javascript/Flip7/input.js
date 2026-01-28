946f1c0af313393f25ef313ff051999e27419025
rmalambamb <ruth.malamba-mboumba@insa-lyon.fr>
2026-01-28 10:18:43 +0100

    Ajout du code qui permet d'afficher une question

--- Javascript/Flip7/input.js
+++ Javascript/Flip7/input.js
@@ -1,4 +1,22 @@
-askPlayerChoice()
+const readline = require("readline")
+
+const rl =readline.createInterface({input: process.stdin, output :process.stdout}) //permet de recuperer la demande de l'utilisateur
 
 
+// fonction qui pinermet d'afficher une question
+function askQuestion(question) {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer.trim());
+    });
+  });
+}
+
+
+
+function closeInput() {
+  rl.close();
+}
+
+module.exports = { askQuestion, closeInput };
 //retourne "draw" ou "stop"
