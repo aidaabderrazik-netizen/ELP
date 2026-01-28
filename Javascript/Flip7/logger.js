9be65c1ea75f56d62766fdaccfcd625c4e7784f3
ruth-mlmb <mboumbaemmanuella@gmail.com>
2026-01-29 00:44:15 +0100

    Update logger.js

--- Javascript/Flip7/logger.js
+++ Javascript/Flip7/logger.js
@@ -7,7 +7,14 @@ const LOG_DIR = path.join(__dirname, 'logs');
 const LOG_FILE = path.join(LOG_DIR, 'game_log.txt');
 
 
+// initialise le fichier au lancement du jeu
+function initLogger() {
+    fs.writeFileSync(LOG_FILE, "===== NOUVELLE PARTIE FLIP7 =====\n", { flag: "w" });
+}
+
 function log(message) {
   const line = message + '\n';
   fs.appendFileSync(LOG_FILE, line, { encoding: 'utf8' });
 }
+
+module.exports={log,initLogger}
