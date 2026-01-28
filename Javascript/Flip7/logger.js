fd7ede04c8020c71248d4a1a766ce1a2cacf9126
Slack0ff <122684258+Slack0ff@users.noreply.github.com>
2026-01-28 11:47:36 +0100

    Update logger.js

--- Javascript/Flip7/logger.js
+++ Javascript/Flip7/logger.js
@@ -1,4 +1,14 @@
-log(message)
+const fs = require('fs');
+const path = require('path');
 
 
-//écrit dans logs/game_log.txt
+// Chemin du dossier et du fichier de log
+const LOG_DIR = path.join(__dirname, 'logs');
+const LOG_FILE = path.join(LOG_DIR, 'game_log.txt');
+
+
+function log(message) {
+  ensureLogDir();
+  const line = message + '';
+  fs.appendFileSync(LOG_FILE, line, { encoding: 'utf8' });
+}
