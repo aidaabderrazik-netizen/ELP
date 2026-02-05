623eba3f44b4ab844ff84449d57bd752e8e8c8e5
ruth-mlmb <mboumbaemmanuella@gmail.com>
2026-02-05 19:33:19 +0100

    Update logger.js

--- Javascript/logger.js
+++ Javascript/logger.js
@@ -1,20 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-
+const fs = require("fs");
+const path = require("path");
 
 // Chemin du dossier et du fichier de log
-const LOG_DIR = path.join(__dirname, 'logs');
-const LOG_FILE = path.join(LOG_DIR, 'game_log.txt');
-
+const LOG_DIR = path.join(__dirname, "logs");
+const LOG_FILE = path.join(LOG_DIR, "game_log.txt");
 
 // initialise le fichier au lancement du jeu
 function initLogger() {
-    fs.writeFileSync(LOG_FILE, "===== NOUVELLE PARTIE FLIP7 =====\n", { flag: "w" });
+
+  // si le dossier logs n'existe pas, on le crée
+  if (!fs.existsSync(LOG_DIR)) {
+    fs.mkdirSync(LOG_DIR);
+  }
+
+  fs.writeFileSync(LOG_FILE, "===== NOUVELLE PARTIE FLIP7 =====\n", { flag: "w" });
 }
 
 function log(message) {
-  const line = message + '\n';
-  fs.appendFileSync(LOG_FILE, line, { encoding: 'utf8' });
+  const line = message + "\n";
+  fs.appendFileSync(LOG_FILE, line, { encoding: "utf8" });
 }
 
-module.exports={log,initLogger}
+module.exports = { log, initLogger };
